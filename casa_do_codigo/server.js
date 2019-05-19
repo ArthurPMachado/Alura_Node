96eb2b6ab9554dc6aa6bffb7e3f3d09b6de55259
Arthur Pereira Machado <machadoparthur1@gmail.com>
2019-05-19 03:09:05 -0300

    Iniciando com node, npm e express

---
+++ casa_do_codigo/server.js
@@ -0,0 +1,38 @@
+const http = require('http');
+// import {createServer} from 'http' - ES6
+// código http.createServer passa a ser apenas createServer
+
+const servidor = http.createServer((req, resp) => {
+    let html = '';
+    if (req.url == '/'){
+        html = `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Casa do Código </h1>
+                </body> 
+            </html>
+        `;
+    } else if (req.url == '/livros'){
+        html = `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Listagem de livros </h1>
+                </body> 
+            </html>
+        `;
+    }
+
+    resp.end(html);
+});
+
+servidor.listen(3000);
+
+function newFunction() {
+    return require('http');
+}
